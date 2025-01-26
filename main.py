223ddbd7c317a20ff17cd59ed55deeb247891604
Techie Ernie <techieernie@gmail.com>
2025-01-26 09:50:04 +0800

    name changes - adding support for VCT matches via rib.gg soon

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import configparser
 import time
+import os
 from youtube_download import download_youtube
-from extract_images import extract_images
-from extract_video import convert_rounds, extract_clip
+from comp_extract_images import extract_images
+from comp_extract_video import convert_rounds, extract_clip
 from scraper import scrape_stats
-from edit import edit_video, get_predictions
-from find_match_stats import predict_map_name, check_score, search_score
+from comp_edit import edit_video, get_predictions
+from comp_find_match_stats import predict_map_name, check_score, search_score
 
 start_time = time.time()
+
+assert os.environ.get("API_KEY") is not None, "No API KEY!"
 
 
 def read_config():
@@ -28,28 +31,32 @@ def read_config():
 
 
 filename = download_youtube(input("YouTube URL: "))
-# stats_link = input("Stats link (valorant.op.gg)")
-player_id = input("Player ID: ")
-score = check_score(filename)
-print(score)
-map_name = predict_map_name(filename)
-print(map_name)
-stats_link = search_score(map_name, player_id, score)
 
+vct_or_comp = input("VCT/COMP: ")
+if vct_or_comp == "COMP":
+    # stats_link = input("Stats link (valorant.op.gg)")
+    player_id = input("Player ID: ")
+    score = check_score(filename)
+    print(score)
+    map_name = predict_map_name(filename)
+    print(map_name)
+    stats_link = search_score(map_name, player_id, score)
 
-if filename and stats_link:
-    config = read_config()
-    highlights_dict = scrape_stats(stats_link, config["minimum_kills"])
-    if highlights_dict:
-        score_dict = extract_images(filename, frame_interval=config["interval"])
+    if filename and stats_link:
+        config = read_config()
+        highlights_dict = scrape_stats(stats_link, config["minimum_kills"])
+        if highlights_dict:
+            score_dict = extract_images(filename, frame_interval=config["interval"])
 
-        round_dict = convert_rounds(score_dict)
-        if round_dict:
-            video_count = extract_clip(filename, round_dict, highlights_dict)
-            print(video_count)
-            for i in range(video_count):
-                print(i)
-                pred = get_predictions(f"video{i}.mp4", debug=False)
-                edit_video(predictions=pred, video_path=f"video{i}.mp4")
-                end_time = time.time()
-                time_taken = end_time - start_time
+            round_dict = convert_rounds(score_dict)
+            if round_dict:
+                video_count = extract_clip(filename, round_dict, highlights_dict)
+                print(video_count)
+                for i in range(video_count):
+                    print(i)
+                    pred = get_predictions(f"video{i}.mp4", debug=False)
+                    edit_video(predictions=pred, video_path=f"video{i}.mp4")
+                    end_time = time.time()
+                    time_taken = end_time - start_time
+else:
+    stats_link = input("Stats link(rib.gg)")
