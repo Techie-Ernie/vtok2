d786938d753e099318bb36d70fd78b8f9bf54593
Techie Ernie <techieernie@gmail.com>
2025-01-28 11:34:01 +0800

    bug fixes with ocr

--- vct_extract_images.py
+++ vct_extract_images.py
@@ -1,9 +1,21 @@
 import cv2
 import time
-from comp_ocr import ocr
+from vct_ocr import ocr
 
 # VCT EXTRACT IMAGES
 # May require additional code since replays / timeouts show
+
+
+def is_valid_score_change(self_score, prev_self_score, enemy_score, prev_enemy_score):
+    if (
+        abs(int(self_score) - prev_self_score) == 1
+        and int(enemy_score) == prev_enemy_score
+    ) or (
+        abs(int(enemy_score) - prev_enemy_score) == 1
+        and int(self_score) == prev_self_score
+    ):
+        return True
+    return False
 
 
 # Returns score dict
@@ -18,13 +30,13 @@ def extract_images(video_path, output_dir="images/", frame_interval=540, debug=T
     added_frames = []
     score_dict = {}
     frame_number = 0
-
+    prev_self_score = 0
+    prev_enemy_score = 0
     while True:
         ret, frame = cap.read()
         if not ret:
             break
-        prev_self_score = 0
-        prev_enemy_score = 0
+
         round = 0
         if frame_number % frame_interval == 0:
             timestamp = frame_number / fps
@@ -32,25 +44,33 @@ def extract_images(video_path, output_dir="images/", frame_interval=540, debug=T
             if frame_number == 0:
                 result_1 = "0"
                 result_2 = "0"
+                score_dict["0:0"] = timestamp_str
             else:
-                cropped_frame_1 = frame[20:70, 780:860]
-                cropped_frame_2 = frame[0:100, 1000:1150]
+                cropped_frame_1 = frame[0:70, 640:1290]
+                # cropped_frame_2 = frame[0:100, 1000:1150]
                 # output_path = os.path.join(output_dir, f"{timestamp_str}.png")
                 img_1 = cv2.resize(
                     (cropped_frame_1), None, fx=2, fy=2, interpolation=cv2.INTER_LINEAR
                 )
-                img_2 = cv2.resize(
-                    (cropped_frame_2), None, fx=2, fy=2, interpolation=cv2.INTER_LINEAR
-                )
 
-                result_1 = ocr(img_1)
-                result_2 = ocr(img_2)
+                # img_2 = cv2.resize(
+                #    (cropped_frame_2), None, fx=2, fy=2, interpolation=cv2.INTER_LINEAR
+                # )
+
+                results = ocr(img_1)
+
+                result_1 = results[0]
+                result_2 = results[1]
+
+                # result_2 = ocr(img_2)
+
                 if debug:
                     print(f"result1:{result_1}")
                     print(f"result2:{result_2}")
                     cv2.imwrite("data/file1.png", img_1)
-                    cv2.imwrite("data/file2.png", img_2)
+
             count = 0
+
             if result_1 is None and result_2 is None:
                 print("Both none")
                 # use ocr to check if it's replay/tech pause/halftime
@@ -62,29 +82,53 @@ def extract_images(video_path, output_dir="images/", frame_interval=540, debug=T
             for item in result:
                 if isinstance(item, str):
                     count += 1
-
+            # Clean up this chunk of code
             if count == 2:  # Check if there are 2 elements e.g. ['0', '0']
-                self_score = result[0]
-                enemy_score = result[1]
+
+                self_score = result_1
+                enemy_score = result_2
+                if debug:
+                    print(f"Previous enemy score: {prev_enemy_score}")
+                    print(f"Previous self_score: {prev_self_score}")
+                    print(f"Self score: {self_score}")
+                    print(f"Enemy score: {enemy_score}")
                 if (
-                    self_score.isdigit() or enemy_score.isdigit()
+                    self_score.isdigit() and enemy_score.isdigit()
                 ):  # Check both are valid integers
                     # can do some additional checking here to see if numbers make sense
-                    if (
-                        # this check ensures that replays are not saved
-                        int(self_score) > prev_self_score
-                        or int(enemy_score) > prev_enemy_score
+
+                    # If self_score is greater than previous_self_score, then enemy score has to be the same
+                    # Similarly, if enemy_score is greater than previous_enemy_score, then self score has to be the same
+                    if int(self_score) != 100 and int(enemy_score) != 100:
+                        if is_valid_score_change(
+                            self_score, prev_self_score, enemy_score, prev_enemy_score
+                        ):
+                            print("check succeeded")
+                            if f"{self_score}:{enemy_score}" not in added_frames:
+                                added_frames.append(f"{self_score}:{enemy_score}")
+                                score_dict[f"{self_score}:{enemy_score}"] = float(
+                                    timestamp_str
+                                )
+                                round += 1
+
+                        prev_self_score = int(self_score)
+                        prev_enemy_score = int(enemy_score)
+                    elif is_valid_score_change(
+                        enemy_score, prev_self_score, self_score, prev_enemy_score
                     ):
-                        if f"{self_score}:{enemy_score}" not in added_frames:
-                            added_frames.append(f"{self_score}:{enemy_score}")
-                            score_dict[f"{self_score}:{enemy_score}"] = float(
+                        # Special case in which the OCR somehow reads the numbers swapped (e.g. 5:4 as 4:5)
+                        print("check succeeded with swap")
+                        if f"{enemy_score}:{self_score}" not in added_frames:
+                            added_frames.append(f"{enemy_score}:{self_score}")
+                            score_dict[f"{enemy_score}:{self_score}"] = float(
                                 timestamp_str
                             )
-                            prev_self_score = int(self_score)
-                            prev_enemy_score = int(enemy_score)
                             round += 1
-
+                    else:
+                        round += 1
         frame_number += 1
+        # print(score_dict)
+
     end = time.time()
     print(
         f"Extraction completed in {end - start} seconds"
--- vct_ocr.py
+++ vct_ocr.py
@@ -4,10 +4,23 @@ reader = easyocr.Reader(["en"])
 
 
 def ocr(img):
+    scores = []
     if len(reader.readtext(img, detail=0)) > 0:
-        return reader.readtext(img, detail=0)[0]
+        results = reader.readtext(img, detail=0)
+        for result in results:
+            if len(result) <= 2 and len(scores) < 2:
+                # replacement for 4 since '4' is detected as 'R'
 
-    return None
+                if "R" in result:
+                    scores.append("4")
+                if result.isdigit():
+                    scores.append(result)
+
+        if len(scores) == 2:
+            return scores
+        else:
+            return ["100", "100"]
+    return ["100", "100"]
 
 
 if __name__ == "__main__":
